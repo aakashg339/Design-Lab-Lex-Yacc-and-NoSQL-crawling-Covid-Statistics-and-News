f855aa5013dc0670b553a15588e3b2b2367d472b
Rajdeep Ghosh <rajdeepghosh@Rajdeeps-MacBook-Air.local>
2024-02-09 11:51:44 +0530

    menu for part a | 3.1

--- Module3.1/A/mapper.py
+++ Module3.1/A/mapper.py
@@ -4,12 +4,17 @@ import sys
 
 datafile=sys.argv[1]
 country_name=sys.argv[2]
+field_name=sys.argv[3]
 
 with open(country_name, 'r') as file:
     for line in file:
         line = line.strip()
         print(f"{line}")
-
+with open(field_name, 'r') as file:
+    for line in file:
+        line = line.strip()
+        print(f"{line}")
+print(f'#####')
 with open(datafile, 'r') as file:
     for line in file:
         line = line.strip()
--- Module3.1/A/reducer.py
+++ Module3.1/A/reducer.py
@@ -4,25 +4,30 @@ import sys
 
 
 def per(a, b):
-    if a==None or b==None:
+    if a == None or b == None:
         return None
-    a=a.replace(',','')
-    b=b.replace(',','')
+    a = a.replace(',', '')
+    b = b.replace(',', '')
     a = float(a)
     b = float(b)
     return round((a / b) * 100, 2)
 
+
 current_key = None
 current_count = 0
 i = 0
+reqd_fields = []
 dict = {}
 for line in sys.stdin:
     if i == 0:
         country_output = line
         country_output = country_output.strip()
+        print(f'Country: {country_output}')
         i += 1
         continue
     if i == 1:
+        line = (line.strip().split('\t'))
+        reqd_fields = eval(line[0])
         i += 1
         continue
     if i == 2:
@@ -31,16 +36,26 @@ for line in sys.stdin:
         i += 1
         continue
     country, data = line.strip().split('\t')
+    # print(f'{country}\t{data}')
     data = eval(data)
-    dict[country] = {lebels[0]: data[0], lebels[6]: data[6], lebels[2]: data[2], lebels[4]: data[4], lebels[10]: data[10], lebels[9]: data[9], lebels[11]: data[11], lebels[1]: data[1], lebels[3]: data[3], lebels[5]: data[5]}
-    
+    dict[country] = {lebels[0].lower(): data[0], lebels[6].lower(): data[6], lebels[2].lower(): data[2], lebels[4].lower(): data[4], lebels[10].lower()
+        : data[10], lebels[9].lower(): data[9], lebels[11].lower(): data[11], lebels[1].lower(): data[1], lebels[3].lower(): data[3], lebels[5].lower(): data[5]}
 
-print(f'DETAILS OF {country_output} AND WORLD\'s PERCENTAGE')
-# print(dict[country_output])
-# print(dict['World'])
-for x in dict[country_output]:
-    cc=dict[country_output][x]
-    cw=dict['World'][x]
+# print(dict)
+for i in range(len(reqd_fields)):
+    reqd_fields[i] = reqd_fields[i].strip().lower()
+
+# print(dict)
+
+xx=''
+print(f'DETAILS OF {xx:<20}{country_output:<20} AND WORLD\'s PERCENTAGE\n')
+
+reqd_country_det=dict[country_output]
+world_det=dict['World']
+
+for x in reqd_fields:
+    cc=reqd_country_det[x]
+    cw=world_det[x]
     res=per(cc,cw)
     ss="None"
     if res!=None:
